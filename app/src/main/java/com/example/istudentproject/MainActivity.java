4427b1f9006fde9cb9eaa28b3fa886b164b16c26
jdoming8 <jillian_dominguez@brown.edu>
2023-04-21 22:10:50 +0200

    UI components done, no functionality yet

--- app/src/main/java/com/example/istudentproject/MainActivity.java
+++ app/src/main/java/com/example/istudentproject/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.istudentproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        // Find views by their ID
+        EditText emailEditText = findViewById(R.id.email_edit_text);
+        EditText passwordEditText = findViewById(R.id.password_edit_text);
+        Button loginButton = findViewById(R.id.login_button);
+        TextView registerLinkText = findViewById(R.id.register_link_text);
+
+        // Set listeners for the views as needed
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle login button click
+            }
+        });
+
+        registerLinkText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle register link click
+            }
+        });
     }
 }
